3cc4eabf308548b5897d0098378d2dbc0ab6f8e7
Campus <Campus@192.168.3.166>
2013-10-18 09:59:08 +0200

    Añadir suma

---
+++ Fusion/src/fusion/Fusion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fusion;
+
+/**
+ *
+ * @author Campus
+ */
+public class Fusion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        int a = 3, b = 5;
+        int suma = a +b;
+    }
+}
