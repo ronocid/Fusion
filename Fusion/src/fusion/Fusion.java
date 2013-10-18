079beaf44069a4ac99372a560ce82cf1ecacb71a
ronocid <ronocid@campusaula.com>
2013-10-18 11:01:33 +0200

    Update Fusion.java

--- Fusion/src/fusion/Fusion.java
+++ Fusion/src/fusion/Fusion.java
@@ -4,10 +4,7 @@ public class Fusion {
 
     public static void main(String[] args) {
       
-<<<<<<< HEAD
         System.out.println(" Hola mundo..");
-=======
         System.out.println(" Hola paco");
->>>>>>> origin/master
     }
 }
