25c5459294e2983d65c8702c6d6b9a834239cd1c
Campus <Campus@192.168.3.166>
2013-10-18 10:49:51 +0200

    Comentar

--- Fusion/src/fusion/Fusion.java
+++ Fusion/src/fusion/Fusion.java
@@ -15,7 +15,6 @@ public class Fusion {
      */
     public static void main(String[] args) {
       
-        int a = 3, b = 5;
-        int suma = a +b;
+        System.out.println(" Hola mundo");
     }
 }
